3d1c12910d2a7c36711544a7dcb27c99a76b7fff
Turing99 <andrei.fratila99@yahoo.ro>
2021-07-27 17:52:00 +0300

    Jdbc

---
+++ Spring/Connecting_to_database(JDBC)/src/main/java/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import config.ProjectConfig;
+import model.Product;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import repositories.ProductRepository;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (var c = new AnnotationConfigApplicationContext(ProjectConfig.class))
+        {
+            ProductRepository  productRepository = c.getBean(ProductRepository.class);
+//
+//            Product product = new Product();
+//            product.setName("Beer");
+//            product.setPrice("7");
+//
+//            productRepository.addProduct(product);
+
+            List<Product> products = productRepository.getProducts();
+            products.forEach(System.out::println);
+        }
+
+    }
+}
---
+++ Spring/Connecting_to_database(JDBC)/src/main/java/model/Product.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public Product setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Product setPrice(double price) {
+        this.price = price;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
