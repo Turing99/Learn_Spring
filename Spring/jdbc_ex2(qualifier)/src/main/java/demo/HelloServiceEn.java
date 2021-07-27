a03d1fce7de4fddd95d52bc418f1fa487e044d71
Turing99 <andrei.fratila99@yahoo.ro>
2021-07-27 18:47:41 +0300

    qualifier

---
+++ Spring/jdbc_ex2(qualifier)/src/main/java/demo/HelloServiceEn.java
@@ -0,0 +1,13 @@
+package demo;
+import demo.HelloService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("en")
+public class HelloServiceEn implements HelloService {
+
+    public String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+}
